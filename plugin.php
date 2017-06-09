9a44be581ef79e0f4c0d8abae4893a4addafa9d3
Victor M. Font Jr <victor@victorfont.com>
2017-06-09 08:56:23 -0400

    Added recommended license block

--- plugin.php
+++ plugin.php
@@ -14,6 +14,21 @@
  *
  */
 
+/*
+Classic Text Widget is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+Classic Text Widget is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with Classic Text Widget. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
+
 namespace VictorFontCG;
 
 if( ! defined( 'ABSPATH' ) ) exit;
